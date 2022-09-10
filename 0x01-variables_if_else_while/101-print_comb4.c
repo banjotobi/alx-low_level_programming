5dae986bd087f7346dd9f1c6aa7617eba9673e79
Oluwatobi Banjo <banjotobi@gmail.com>
2022-09-09 19:15:13 -0700

    added digit3 check

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,7 +20,7 @@ int main(void)
 				putchar((digit2 % 10) + '0');
 				putchar((digit3 % 10) + '0');
 
-				if (digit1 == 8 && digit2 == 9)
+				if (digit1 == 8 && digit2 == 9 && digit3 == 9)
 					continue;
 
 				putchar(',');
