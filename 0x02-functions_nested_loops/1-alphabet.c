75fc4ba3464761e9d7fe5f2ebba15c40bdd0df48
Oluwatobi Banjo <banjotobi@gmail.com>
2022-09-14 21:10:52 -0700

    print alphabets a - z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - returns teh alphabets in lower case
+ */
+
+void main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
