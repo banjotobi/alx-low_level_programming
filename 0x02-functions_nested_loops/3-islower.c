76c511a462d59d24e914d6b471c6c732ffdc74a9
Oluwatobi Banjo <banjotobi@gmail.com>
2022-09-14 21:36:58 -0700

    check if lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower -  checks if a character is lower case
+ * @c: The character to be checked
+ *
+ * Return: 1 if lower, 0 if capital
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && <= 'z')
+		return (1);
+	else
+		return (0);
+}
