c51cac04751ee55b2d23c38b3cc195958584962e
Oluwatobi Banjo <banjotobi@gmail.com>
2022-09-14 23:01:59 -0700

    check if uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * isupper - check if characer is capital lettr
+ * @c the character being checkd
+ *
+ * Return: 1 if CAPITAL, 0 not capital
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File - MAIN.h
+ * 
+ * Desc: Header file containing declarations for all funcitons
+ */
+
+int _isupper(int c);
+
+#endif
