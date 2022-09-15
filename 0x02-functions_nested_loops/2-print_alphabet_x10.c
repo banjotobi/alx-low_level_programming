28bc3946f442b34c5b331d85669e9f7c9c40079e
Oluwatobi Banjo <banjotobi@gmail.com>
2022-09-14 21:29:31 -0700

    fixed betty issues

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabets in lower case 10 times
+ * print_alphabet_x10 - prints the alphabets in lower case 10 times
  */
 
 void print_alphabet_x10(void)
@@ -9,11 +9,10 @@ void print_alphabet_x10(void)
 	char letter;
 	int count = 1;
 
-	while(count++ <=  10)
+	while (count++ <= 10)
 	{
-		for(letter = 'a'; letter <= 'z'; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-
 		_putchar('\n');
 	}
 }
