2642c9176b9b4a394b571546ff3fb97703d097dc
Oluwatobi Banjo <banjotobi@gmail.com>
2022-09-15 21:47:50 -0700

    fixed betty

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - testing of the ability of a function to determine if a number is
- * 		positive or negative
+ *positive or negative
  *
  * Return: 0
  */
@@ -11,7 +11,8 @@ int main(void)
 {
 	int i;
 	i = 0;
+
 	positive_or_negative(i);
 
 	return (0);
-}	
+}
