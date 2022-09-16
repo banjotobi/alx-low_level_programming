c3a1f9aec7019329d1ed05428f4501e51f13f70f
Oluwatobi Banjo <banjotobi@gmail.com>
2022-09-15 23:02:12 -0700

    added modulus

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,6 @@
 /**
  * print_numbers - print numbers 0 - 9
  *
- * Return: value of multiplication
  */
 
 void print_numbers(void)
@@ -11,6 +10,6 @@ void print_numbers(void)
 	int i;
 
 	for (i = 0; i <= 9; i++)
-		_putchar(i + '0');
+		_putchar((i % 10) + '0');
 	_putchar('\n');
 }
