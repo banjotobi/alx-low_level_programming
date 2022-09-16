dca11fec6e40287a0a559519ce89900ecb5b760c
Oluwatobi Banjo <banjotobi@gmail.com>
2022-09-15 23:17:33 -0700

    fixed

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - print numbers 0 - 9 without printing 2, 4
- *
+ * print_most_numbers - print numbers 0 - 9 without printing 2, 4
  */
 
 void print_most_numbers(void)
