5b3986b59bdb2ac435e43fa5ae5aa9ee2a5d0ea8
Oluwatobi Banjo <banjotobi@gmail.com>
2022-09-15 22:11:39 -0700

    added isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * isupper - check for digit (0-9)
+ * @c the number being checkd
+ *
+ * Return: 1 if DIIGT, 0 not DIGIT
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
