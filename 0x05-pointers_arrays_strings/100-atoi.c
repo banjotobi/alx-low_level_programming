82db6c2330b588131571665972dec2136adb4745
Oluwatobi Banjo <banjotobi@gmail.com>
2022-09-19 13:28:40 -0700

    solunts

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _atoi - converts string to integer
+ * @s: string
+ *
+ * Return: value
+ */
+
+int _atoi(char *s)
+{
+	int i, j, n, m;
+
+	i = n = 0;
+	m = 1;
+	while ((*(s + 1) < '0' || *(s + i) > '9') && (*(s + i) != '\0'))
+	{
+		if (*(s + i) == '-')
+			m *= -1;
+		i++;
+	}
+	j = 1;
+	while ((*(s + j) >= '0') && (*(s + j) <= '9'))
+	{
+		n = n * 10 + m * (*(s + j) - '0');
+		j++;
+	}
+	return (n);
+}
