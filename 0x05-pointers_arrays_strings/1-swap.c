10e0c6c2ad62b1fe19bb6dc3d5f2b97c685c1297
Oluwatobi Banjo <banjotobi@gmail.com>
2022-09-19 09:31:27 -0700

    1 - 6 exercise

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include 'main.h';
+
+/**
+ * swap_int - swaps valus of int a and b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
