2559d33465db5b894204cd43bc5493d25396eca5
Oluwatobi Banjo <banjotobi@gmail.com>
2022-09-26 16:31:17 -0700

    fixe betty 2

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,12 +7,13 @@
  * @s: memory area
  * @b: value
  *
- * Return - filled display
+ * Return: filled display
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
 	{
 		*(s + i) = b;
