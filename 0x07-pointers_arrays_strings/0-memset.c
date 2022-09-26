d1d55151ea471ec1b37d40f4c57d7767c642c147
Oluwatobi Banjo <banjotobi@gmail.com>
2022-09-26 05:09:02 -0700

    _memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with byte
+ *
+ * n: number of spaces
+ * s: memory area
+ * b: value
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * FILE - MAIN.H
+ *
+ * Desc: Header file
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
